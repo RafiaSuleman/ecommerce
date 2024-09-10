e60073e927e102948d313ea1fc2cc15d25f4c3ff
RafiaSuleman <rafiasuleman47@gmail.com>
2024-09-10 17:39:42 +0500

    commit-13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "../components/navbar";
-import CartProvider from "@/components/providers";
-
+import Navbar from "@/components/ui/navbar";
+import CartProvider from "@/components/ui/providers";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
---
+++ src/components/ui/ShoppingCartModal.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useShoppingCart } from "use-shopping-cart";
+
+export default function ShoppingCartModal() {
+  const {
+    cartCount,
+    shouldDisplayCart,
+    handleCartClick,
+  } = useShoppingCart();
+
+ 
+  return (
+    // <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
+    <Sheet defaultOpen>
+      <SheetContent className="sm:max-w-lg w-[90vw]">
+        <SheetHeader>
+          <SheetTitle>Shopping Cart</SheetTitle>
+        </SheetHeader>
+        <div className="h-full flex flex-col justify-between">
+          <div className="mt-8 flex-1 overflow-y-auto">
+            <ul className="-my-6 divide-y divide-gray-200">
+              {cartCount === 0 ? (
+                <h1 className="py-6">You dont have any items</h1>
+              ) : (
+                " Thanks for shopping"
+              )}
+            </ul>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
